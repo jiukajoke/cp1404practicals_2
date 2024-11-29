fb7fc39378a598c0a8b5a5f27035128418263e30
joke03 <93513471+joke03@users.noreply.github.com>
2024-11-29 15:35:16 +0800

    Complementary intersection of Prac8

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_clear(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
+
+    def handle_greet(self):
+        name = self.root.ids.input_name.text
+        if name:
+            self.root.ids.output_label.text = f"Hello {name}"
+        else:
+            self.root.ids.output_label.text = 'Please enter your name'
+
+if __name__ == '__main__':
+    BoxLayoutDemo().run()
